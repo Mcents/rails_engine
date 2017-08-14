ea2475650bd5e8e7f8c27cff86e644c92cc01b05
Jim Szalewski <jim.szalewski@gmail.com>
2017-08-14 16:34:01 -0600

    rewrite migrations in proper order

--- db/migrate/20170814223020_create_invoices.rb
+++ db/migrate/20170814223020_create_invoices.rb
@@ -2,8 +2,8 @@ class CreateInvoices < ActiveRecord::Migration[5.1]
   def change
     enable_extension('citext')
     create_table :invoices do |t|
-      t.references :customer_id, foreign_key: true
-      t.references :merchant_id, foreign_key: true
+      t.references :customer, foreign_key: true
+      t.references :merchant, foreign_key: true
       t.citext :status
 
       t.timestamps
