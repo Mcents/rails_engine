acc2ca42dc7451fe9bb838f98e2b19e1bd0a87ea
Jim Szalewski <jim.szalewski@gmail.com>
2017-08-15 13:35:37 -0600

    create show route and action for invoices

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -12,4 +12,15 @@ describe "Invoices API" do
 
     expect(invoices.count).to eq(3)
   end
+
+  it "can get one invoice by its id" do
+    id = create(:invoice).id
+
+    get "/api/v1/invoices/#{id}"
+
+    invoice = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice["id"]).to eq(id)
+  end
 end
