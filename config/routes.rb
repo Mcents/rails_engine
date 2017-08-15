923fdad2afa3472cc13f328ddba87344819e3c74
Michael Centrelli <Acentrelli@gmail.com>
2017-08-15 14:51:50 -0600

    Pass Test for Transaction Random

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
       namespace :transactions do
         get '/find', to: 'find#show'
         get '/find_all', to: 'find#index'
+        get '/random', to: 'random#show'
       end
      resources :transactions, only: [:index, :show]
      resources :merchants, only: [:index, :show]
