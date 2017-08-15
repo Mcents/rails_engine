119ddbee3ac352c4fc7ba1795c20e14e9bc9197a
Jim Szalewski <jim.szalewski@gmail.com>
2017-08-15 15:55:17 -0600

    random item implemented

---
+++ spec/requests/api/v1/items/random_request_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Random item' do
+  context 'items/random.json' do
+    it 'returns a random item' do
+      item = create_list(:item, 3)
+
+      get '/api/v1/items/random.json'
+
+      item_json = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(item_json.count).to eq(1)
+    end
+  end
+end
