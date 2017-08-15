6c0d3de4b35b50500611851996258a9f907e0922
Michael Centrelli <Acentrelli@gmail.com>
2017-08-15 11:49:25 -0600

    Add Test For Merchants Show Endppoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      namespace :merchants do
+      get '/find', to: 'find#show'
+      end
+
+     resources :merchants, only: [:index, :show]
     end
   end
 end
