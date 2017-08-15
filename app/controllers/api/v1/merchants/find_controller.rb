e6698eb6975e6e4594134fc37b830e177d3bc674
Michael Centrelli <Acentrelli@gmail.com>
2017-08-15 12:30:34 -0600

    Test Pass for Merchant Find Index

--- app/controllers/api/v1/merchants/find_controller.rb
+++ app/controllers/api/v1/merchants/find_controller.rb
@@ -3,6 +3,11 @@ class Api::V1::Merchants::FindController < ApplicationController
   def show
     render json: Merchant.find_by(merchant_params)
   end
+
+  def index
+    render json: Merchant.where(merchant_params)
+  end
+
   private
 
   def merchant_params
--- spec/requests/api/v1/merchants/find_request_spec.rb
+++ spec/requests/api/v1/merchants/find_request_spec.rb
@@ -13,4 +13,18 @@ describe "merchants finder" do
       expect(merchant_json["name"]).to eq(merchant.name)
     end
   end
+
+  context "merchant/find_all?params" do
+    it "returns all merchants with params" do
+      merchant1 = create_list(:merchant, 3, name: "mike")
+      merchant2 = create_list(:merchant, 2, name: "john")
+
+      get "/api/v1/merchants/find_all?name=#{merchant1.first.name}"
+      item_json = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(item_json.first["name"]).to eq("mike")
+      expect(item_json.count).to eq(3)
+    end
+  end
 end
