b0b61e77d00d57d11e45706bd8b20ad1cb91e413
Jim Szalewski <jim.szalewski@gmail.com>
2017-08-15 11:57:23 -0600

    fix associations in model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,6 @@
 class Invoice < ApplicationRecord
-  belongs_to :customer
-  belongs_to :merchant
+  belongs_to :customer, required: false
+  belongs_to :merchant, required: false
   has_many :invoice_items
   has_many :items, through: :invoice_items
   has_many :transactions
