adbda0804e2ac025859fa69b10be9fdee28bc458
Jim Szalewski <jim.szalewski@gmail.com>
2017-08-15 17:01:05 -0600

    find and find_all for invoice items

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,12 @@ Rails.application.routes.draw do
         get '/random', to: 'random#show'
       end
 
+      namespace :invoice_items do
+        get '/find', to: 'find#show'
+        get '/find_all', to: 'find#index'
+        get '/random', to: 'random#show'
+      end
+
      resources :items, only: [:index, :show]
      resources :transactions, only: [:index, :show]
      resources :merchants, only: [:index, :show]
