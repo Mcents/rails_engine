efc4d8f43a9695f6a4bcc3ac34a478fa9c0a9d2c
Michael Centrelli <Acentrelli@gmail.com>
2017-08-16 14:05:33 -0600

    Test Pass for Merchant Revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,11 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+
+  def find_revenue(date = nil)
+    invoices.joins(:invoice_items, :transactions)
+    .merge(Invoice.date_match(date))
+    .merge(Transaction.successful)
+    .sum("quantity * unit_price")
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice, required: false
+
+
+  def self.successful
+    where(result: "success")
+  end
 end
