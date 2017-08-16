d4176deb949e4d1e515b3c6e84e3984a3ea9077e
Michael Centrelli <Acentrelli@gmail.com>
2017-08-16 11:18:31 -0600

    Pass Test for Invoice Items Item Relationship

--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,7 @@ Rails.application.routes.draw do
         get '/find_all', to: 'find#index'
         get '/random', to: 'random#show'
         get '/:id/invoice', to: 'invoice#index'
+        get '/:id/item', to: 'item#index'
       end
 
      resources :items, only: [:index, :show]
