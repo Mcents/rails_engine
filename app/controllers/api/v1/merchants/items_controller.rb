80c42be3d05f323b6b8d41f4bb9ba8c71f2bd3b7
Jim Szalewski <jim.szalewski@gmail.com>
2017-08-16 12:56:53 -0600

    returns merchants items

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::Merchants::ItemsController < ApplicationRecord
+class Api::V1::Merchants::ItemsController < ApplicationController
 
   def index
     render json: Merchant.find(params[:id]).items
