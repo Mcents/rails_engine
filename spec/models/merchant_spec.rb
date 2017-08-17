091f0c13afa84f306d80a54942a9e683d2ca8732
Michael Centrelli <Acentrelli@gmail.com>
2017-08-17 14:53:50 -0600

    Merchant to Items relationship

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
+  context "relationships" do
+    it "has_many items" do
+      merchant = create(:merchant)
+
+      expect(merchant).to respond_to(:items)
+
+    end
+  end
 end
